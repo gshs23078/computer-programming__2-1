10a853d94c20f807c0a1e06e5e58fa55cb16a98d
gshs23078 <129289574+gshs23078@users.noreply.github.com>
2024-03-17 22:51:54 +0900

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
 int f(int n){
+	if(n==0) return 1;
 	if(n==1) return 1;
-	if(n==2) return 2;
-	if(n==3) return 4;
-	return f(n-1)+f(n-2)+f(n-3);
+	return f(n-1)+f(n-2);
 }
 int main(){
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
 	int n;
 	cin >> n;
 	cout << f(n);
